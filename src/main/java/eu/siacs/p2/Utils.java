6afc7949dd5db78a7feed1f071192b1e966fbd56
Wajed B. Isleem <wajedbisleem@gmail.com>
2020-12-02 00:07:00 +0200

    update

--- src/main/java/eu/siacs/p2/Utils.java
+++ src/main/java/eu/siacs/p2/Utils.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.stream.Collectors;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Utils {
