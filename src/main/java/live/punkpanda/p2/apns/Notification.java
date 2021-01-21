4f42b5a859f60ff886962acf839319687d292280
Wajed B. Isleem <wajed@hagzi.com>
2021-01-21 12:05:07 +0200

    update

--- src/main/java/live/punkpanda/p2/apns/Notification.java
+++ src/main/java/live/punkpanda/p2/apns/Notification.java
@@ -6,55 +6,55 @@ import com.google.gson.annotations.SerializedName;
 import live.punkpanda.p2.xmpp.extensions.push.MessageBody;
 
 public class Notification { 
-    Aps aps;
-    public Notification(String sender, String recevier, MessageBody body){
-        aps = new Aps(sender, recevier, body);
+    public Aps aps = new Aps();
+    public Notification(String sender, String recevier, MessageBody body) {
+        //aps = new Aps(sender, recevier, body);
     }
 }
 
 class Aps {
-    public Alert alert;
+    public Alert alert = new Alert();
     public int badge;
     public String sound= "default";
 
 
-    public Aps(String sender, String recevier, MessageBody body) {
-        String senderName = "";
-        if (!sender.equals("")) {
-            VCardService vCardService = new VCardService();
-            senderName = vCardService.vcard(sender);
-        }
+    // public Aps(String sender, String recevier, MessageBody body) {
+    //     String senderName = "";
+    //     if (!sender.equals("")) {
+    //         VCardService vCardService = new VCardService();
+    //         senderName = vCardService.vcard(sender);
+    //     }
 
-        OfflineService offlineService = new OfflineService();
-        badge = offlineService.offline(recevier);
+    //     OfflineService offlineService = new OfflineService();
+    //     badge = offlineService.offline(recevier);
 
-        alert = new Alert(senderName, body);
+    //     alert = new Alert(senderName, body);
         
-    }
+    // }
 }
 
 class Alert {
-    public String title;
-    public String body;
+    public String title = "wajed";
+    public String body = "hi";
 
-    public Alert(String senderName, MessageBody messagebody) {
-        title = "wajed";
-        body = "hi";
-        // title = senderName.equals("") ? "Group message" : senderName;
-        // if (messagebody != null) {
-        //     if (messagebody.type.equals("text")) {
-        //         body = messagebody.content;
-        //     } else if (messagebody.type.equals("image")) {
-        //         body = "Receive image";
-        //     } else if (messagebody.type.equals("voice")) {
-        //         body = "Receive voice";
-        //     } else if (messagebody.type.equals("video")) {
-        //         body = "Receive video";
-        //     } else if (messagebody.type.equals("file")) {
-        //         body = "Receive file";
-        //     }
-        // } else {
-        //     body = "";
-        // }
-    }
+    // public Alert(String senderName, MessageBody messagebody) {
+    //     title = "wajed";
+    //     body = "hi";
+    //     // title = senderName.equals("") ? "Group message" : senderName;
+    //     // if (messagebody != null) {
+    //     //     if (messagebody.type.equals("text")) {
+    //     //         body = messagebody.content;
+    //     //     } else if (messagebody.type.equals("image")) {
+    //     //         body = "Receive image";
+    //     //     } else if (messagebody.type.equals("voice")) {
+    //     //         body = "Receive voice";
+    //     //     } else if (messagebody.type.equals("video")) {
+    //     //         body = "Receive video";
+    //     //     } else if (messagebody.type.equals("file")) {
+    //     //         body = "Receive file";
+    //     //     }
+    //     // } else {
+    //     //     body = "";
+    //     // }
+    // }
 }
