a7485eb2cb826935271634dad217270b6e4f08b6
Wajed B. Isleem <wajedbisleem@gmail.com>
2021-02-16 18:59:07 +0200

    update

--- src/main/java/live/punkpanda/p2/fcm/FcmPushService.java
+++ src/main/java/live/punkpanda/p2/fcm/FcmPushService.java
@@ -1,5 +1,6 @@
 package live.punkpanda.p2.fcm;
 
+import com.google.gson.Gson;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.GsonBuilder;
 import live.punkpanda.p2.Configuration;
